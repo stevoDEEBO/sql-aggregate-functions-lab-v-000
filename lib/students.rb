0eeebbc0f40fd3acc5c04d8fdf54d0f9640b3cce
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-08-26 02:59:09 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "select min(gpa) from students;"
 end
 
 def average_student_gpa
+  "select avg(gpa) from students;"
 end
 
 def total_tardies_for_all_students
+  "select sum(tardies) from students;"
 end
 
 def average_gpa_for_9th_grade
+  "select max(gpa) from students where grade = 9;"
 end
