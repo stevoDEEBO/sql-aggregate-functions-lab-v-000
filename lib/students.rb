9c673648b93d2f4ff53e2e2143b0dc89750735e6
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-08-26 03:01:43 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -17,5 +17,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-  "select max(gpa) from students where grade = 9;"
+  "select avg(gpa) from students where grade = 9;"
 end
